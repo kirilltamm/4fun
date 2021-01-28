073507a81f7a4b8f256a577d422a3621e4f25885
kirilltamm <k.tamm@bk.ru>
2021-01-28 15:38:18 +0300

    Added sectry sdk

--- FunScript.py
+++ FunScript.py
@@ -1,1 +1,7 @@
-print('hi, fucker') 
+import sentry_sdk
+sentry_sdk.init(
+    "https://94fec882847a485c9fd8a71b1d57598c@o503592.ingest.sentry.io/5612629",
+    traces_sample_rate=1.0
+)
+
+division_by_zero = 1 / 0
